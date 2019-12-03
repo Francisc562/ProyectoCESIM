b0fedbb43063feb897ddcc849611335343b15a06
francisc562 <franciscopp845@gmail.com>
2019-12-03 17:49:44 -0600

    nnn

--- Proyecto_Final_Registro_CESIM/Models/Estudiante.cs
+++ Proyecto_Final_Registro_CESIM/Models/Estudiante.cs
@@ -5,18 +5,14 @@ using System.Threading.Tasks;
 
 namespace Proyecto_Final_Registro_CESIM.Models
 {
-    public enum sex
-    {
-        Masculino, Femenino,
-    }
     public class Estudiante
     {
-        public int estudianteID { get; set; }
+        public int IDESTUDIANTE { get; set; }
         public int tutorID { get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
 
-        public sex? Sexo { get; set; }
+        public string Sexo { get; set; }
        
         public DateTime Nacimiento { get; set; }
         public string Codigo { get; set; }
--- Proyecto_Final_Registro_CESIM/Models/Grado.cs
+++ Proyecto_Final_Registro_CESIM/Models/Grado.cs
@@ -5,19 +5,16 @@ using System.Threading.Tasks;
 
 namespace Proyecto_Final_Registro_CESIM.Models
 {
-    public enum lev
-    {
-        Primero, Segundo, Tercero, Cuarto, Quinto, Sexto
-    }
 
+  
     public class Grado
     {
         public int gradoID { get; set; }
         public int docenteID { get; set; }
-        public lev? Nivel { get; set; }
         
        
-   
+       
+
         public Docente docente { get; set; }
         public ICollection<Matricula> matriculas { get; set; }
 
